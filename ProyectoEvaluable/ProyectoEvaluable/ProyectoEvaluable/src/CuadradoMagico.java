ba64efb700be1b4e8c89d1d5a4351049c3764c75
AlmuHepburn <almu.fg.6@gmail.com>
2026-01-08 20:52:16 +0100

    ok

--- ProyectoEvaluable/ProyectoEvaluable/ProyectoEvaluable/src/CuadradoMagico.java
+++ ProyectoEvaluable/ProyectoEvaluable/ProyectoEvaluable/src/CuadradoMagico.java
@@ -5,6 +5,10 @@ public class CuadradoMagico {
         private Scanner scanner = new Scanner(System.in);
         private int n = 0;
         private int [][] cuadrado;
+        private int sumaFila = 0;
+        private int sumaColumna = 0;
+        private int sumaDiagonal1 = 0;
+        private int sumaDiagonal2 = 0;
 
         public void pedirNumeroUsuario(){
             do{
@@ -20,28 +24,42 @@ public class CuadradoMagico {
 
 
         public void generarCuadrado(){
-            //System.out.println("Introduce el tamaño de la matriz (n): ");
-           // int n = scanner.nextInt();
-           // cuadrado = new int[n][n];
+
+            int intentos = 0;
+            boolean esMagico = false;
+            int constanteMagica = n*(n*n +1)/2;
+
+            do {
+                intentos++;
+                cuadrado = new int[n][n];
+
+
+                for (int i = 0; i < n; i++) {
+                    for (int j = 0; j < n; j++) {
+                        int aleatorio;
+                        do {
+                            aleatorio = (int) (Math.random() * (n * n)) + 1;
+                        } while (estaNumero(aleatorio));
+                        cuadrado[i][j] = aleatorio;
+
+                    }
+
+                }
+                esMagico = verificarCuadrado(constanteMagica);
+            }while(!esMagico);
+
             System.out.println("===CUADRADO MÁGICO ENCONTRADO===");
-            cuadrado = new int[n][n];
-            for (int i = 0; i < cuadrado.length; i++) {
-                for (int j = 0; j < cuadrado[i].length; j++) {
-                    int aleatorio;
-                    do {
-                        aleatorio= (int) (Math.random()*21);
-                    }while(estaNumero(aleatorio));
-                    cuadrado[i][j] = aleatorio;
 
-                }
 
+            for (int[] fila : cuadrado) {
+                    for (int item : fila) {
+                        System.out.print(item + "\t");
+                    }
+                    System.out.println();
             }
-            for (int[] fila: cuadrado){
-                for (int item: fila) {
-                    System.out.print(item+"\t");
-                }
-                System.out.println();
-            }
+
+            System.out.println("Constante mágica: " + constanteMagica);
+            System.out.println("Intentos realizados: " + intentos);
 
 
         }
@@ -57,6 +75,51 @@ public class CuadradoMagico {
             }
 
             return false;
+        }
+
+        private boolean verificarCuadrado(int constanteMagica) {
+
+            for (int i = 0; i < n; i++) {
+                sumaFila=0;
+                for (int j = 0; j < n; j++) {
+                    sumaFila += cuadrado[i][j];
+                }
+                if (sumaFila != constanteMagica) {
+                    return false;
+                }
+
+
+            }
+            for (int j = 0; j < n; j++) {
+                sumaColumna=0;
+                for (int i = 0; i < n; i++) {
+                    sumaColumna += cuadrado[i][j];
+                }
+                if (sumaColumna != constanteMagica) {
+                    return false;
+                }
+
+
+            }
+
+            sumaDiagonal1 = 0;
+            for (int i = 0; i < n; i++) {
+                sumaDiagonal1 += cuadrado[i][i];
+            }
+            if (sumaDiagonal1 != constanteMagica) {
+                return false;
+            }
+
+            sumaDiagonal2 = 0;
+            for (int i = 0; i < n; i++) {
+                sumaDiagonal2 += cuadrado[i][n-1-i];
+            }
+            if (sumaDiagonal2 != constanteMagica) {
+                return false;
+            }
+
+            return true;
+
         }
 
     }
